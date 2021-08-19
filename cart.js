55bc733ac35a814f8ca714881d5412f04acf964f
TerrorMhaule <83818806+TerrorMhaule@users.noreply.github.com>
2021-08-19 10:13:27 +0200

    Update cart.js

--- cart.js
+++ cart.js
@@ -192,7 +192,7 @@ var cart = {
       var total = 0;
       for (let id in cart.items) {
         total += cart.items[id] * products[id].price;
-        document.getElementById("c-total").innerHTML ="TOTAL: $" + total;
+        document.getElementById("c-total").innerHTML ="TOTAL: R" + total;
       }
     }
   },
